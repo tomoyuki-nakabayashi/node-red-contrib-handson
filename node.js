9df42aad5b1964f8a60db224da667d32d8a07283
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2017-08-17 20:40:10 +0900

    first node

---
+++ node.js
@@ -0,0 +1,11 @@
+module.exports = function(RED) {
+    function TomNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("tom-node",TomNode);
+}
